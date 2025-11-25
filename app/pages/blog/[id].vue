ebb471c4e9d0e225e8955ec5677ea1e6eae65dd5
Borys Lopez <borislopez1976@hotmail.com>
2025-11-25 16:00:36 -0400

    PArte 4: useFetch - Consume APIs, Rutas Dinámicas, Manejo de Errores y TypeScript

---
+++ app/pages/blog/[id].vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h1>blog {{ $route.params.id }}</h1>
+        <div v-if="error">
+            <p>{{ error.message }}</p>
+        </div>
+        <div v-else-if="post">
+            <h1>{{ post.title }}</h1>
+            <p>{{ post.body }}</p>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+    import type { Post } from '#shared/types/post';
+    const { id } = useRoute().params
+    const { data: post, error } = await useFetch<Post>(`https://jsonplaceholder.typicode.com/posts/${id}`,{
+        
+    })
+</script>
--- app/pages/blog/index.vue
+++ app/pages/blog/index.vue
@@ -1,11 +1,19 @@
 <template>
     <div>
         <h1>blog page</h1>
+        <ul>
+            <li v-for="post in posts" :key="post.id">
+                <NuxtLink :to="`${post.id}`">
+                    <span>{{ post.title }}</span>
+                </NuxtLink>
+            </li>
+        </ul>
     </div>
 </template>
 
-<script>
-    export default {
+<script setup lang="ts">
+    import type { Post } from '#shared/types/post';
+    const { data: posts } = await useFetch<Post[]>('https://jsonplaceholder.typicode.com/posts',{
         
-    }
+    })
 </script>
